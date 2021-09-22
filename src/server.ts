f6cbabf935303c8323329265140e5a7e88f39af3
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-22 14:04:34 -0300

    eslit commit

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import express from "express";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = express();
+
+app.listen(3333,()=> console.log("Server is Running!"));
