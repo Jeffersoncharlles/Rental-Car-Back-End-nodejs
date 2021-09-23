8b427e6f425693aec34184511d1c37c64284de98
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-23 12:01:04 -0300

    add: estanciando category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,15 +5,23 @@ import { Category } from '../model/Category';
 const categoriesRoutes = Router();
 
 const categories: Category[] = [];
+// categoria e um array de category modal
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    const category: Category = {
+    const category = new Category();
+
+    Object.assign(category, {
         name,
         description,
         created_at: new Date(),
-    };
+    });
+    // isso e mesma coisa de eu da um category.name
+
+    // category.name = name;
+    // category.description = description;
+    // category.created_at = new Date();
 
     categories.push(category);
 
