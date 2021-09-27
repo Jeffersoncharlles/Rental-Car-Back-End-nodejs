288ff0eaf48707362bed7523b5db13a4492f4d80
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-27 14:42:36 -0300

    add: loadcategories

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,13 +1,24 @@
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
+import { ICategoriesRepository } from '../../repository/ICategoriesRepository';
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    loadCategories(file: Express.Multer.File): IImportCategory[] {
         // usar modelo nativo fs
         // usar a funcao createReadStream
         // permite fazer a leitura em partes
         // precisa passar o path do file
         const stream = fs.createReadStream(file.path);
+
+        const categories: IImportCategory[] = [];
 
         // eu crio uma const usando o parse
         // eu posso passar configuracao
@@ -19,8 +30,21 @@ class ImportCategoryUseCase {
         stream.pipe(parseFile);
 
         parseFile.on('data', async (line) => {
-            console.log(line);
+            // ['name','description']
+            // console.log(line)
+            const [name, description] = line;
+            categories.push({
+                name,
+                description,
+            });
         });
+        return categories;
+    }
+
+    execute(file: Express.Multer.File): void {
+        const categories = this.loadCategories(file);
+
+        console.log(categories);
     }
 }
 
