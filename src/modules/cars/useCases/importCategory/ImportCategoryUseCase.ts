65dd235b2d9d7e6d48a04005d27717e4928dbdb4
Jefferson Charlle <contato@jefferdeveloper.com>
2021-09-27 14:56:20 -0300

    add: save cvs em db

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -11,41 +11,64 @@ interface IImportCategory {
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    loadCategories(file: Express.Multer.File): IImportCategory[] {
-        // usar modelo nativo fs
-        // usar a funcao createReadStream
-        // permite fazer a leitura em partes
-        // precisa passar o path do file
-        const stream = fs.createReadStream(file.path);
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            // usar modelo nativo fs
+            // usar a funcao createReadStream
+            // permite fazer a leitura em partes
+            // precisa passar o path do file
+            const stream = fs.createReadStream(file.path);
 
-        const categories: IImportCategory[] = [];
+            const categories: IImportCategory[] = [];
 
-        // eu crio uma const usando o parse
-        // eu posso passar configuracao
-        const parseFile = csvParse({});
+            // eu crio uma const usando o parse
+            // eu posso passar configuracao
+            const parseFile = csvParse({});
 
-        // usar a funcao pipe para
-        // percorre o que ta sendo lindo
-        // e joga para outra funcao arquivo etc
-        stream.pipe(parseFile);
+            // usar a funcao pipe para
+            // percorre o que ta sendo lindo
+            // e joga para outra funcao arquivo etc
+            stream.pipe(parseFile);
 
-        parseFile.on('data', async (line) => {
-            // ['name','description']
-            // console.log(line)
-            const [name, description] = line;
-            categories.push({
-                name,
-                description,
-            });
+            parseFile
+                .on('data', async (line) => {
+                    // ['name','description']
+                    // console.log(line)
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on('end', () => {
+                    resolve(categories);
+                })
+                .on('error', (err) => {
+                    reject(err);
+                });
+            // ele tem que ser colocado em uma promise
+            // e da um . on para da o retorno em resolve
+            // . om para o retorno no error
         });
-        return categories;
     }
 
-    execute(file: Express.Multer.File): void {
-        const categories = this.loadCategories(file);
+    execute = async (file: Express.Multer.File): Promise<void> => {
+        const categories = await this.loadCategories(file);
 
-        console.log(categories);
-    }
+        // console.log(categories);
+
+        // map percorre dado por dado igual o foreach
+        categories.map(async (c) => {
+            const { name, description } = c;
+            const existCategory = this.categoriesRepository.findByName(name);
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name,
+                    description,
+                });
+            }
+        });
+    };
 }
 
 export { ImportCategoryUseCase };
