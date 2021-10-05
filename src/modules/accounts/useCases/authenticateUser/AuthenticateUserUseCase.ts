8a0c5f2b04b73517fd0088f347080c55a27b1231
Jefferson Charlle <contato@jefferdeveloper.com>
2021-10-05 10:23:27 -0300

    add: session routes

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,63 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private userRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        // Usuario existe
+        const user = await this.userRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Email or password incorrect!');
+            // se usuario nao existe
+        }
+
+        // senha esta correta
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error('Email or password incorrect!');
+            // se for senhas diferentes
+        }
+
+        // gerar o jsonwebtoken
+        // md5
+        const token = sign({}, 'c2bce9f305189813992b8ec41217b4bb', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
