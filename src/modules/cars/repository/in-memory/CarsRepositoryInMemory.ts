227486624b3000645e29b12b8c48cfd5c4b5d21c
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-17 20:53:13 -0300

    chore: in-memory cars test repository

--- src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
@@ -1,9 +1,32 @@
+/* eslint-disable import/no-unresolved */
 import { ICreateCarDTO } from '../../dto/ICreateCarDTO';
+import { Car } from '../../infra/typeorm/Car';
 import { ICarsRepository } from '../ICarsRepository';
 
 class CarsRepositoryInMemory implements ICarsRepository {
-    create(data: ICreateCarDTO): Promise<void> {
-        throw new Error('Method not implemented.');
+    cars: Car[] = [];
+    async create({
+        name,
+        description,
+        daily_rate,
+        license_plate,
+        fine_amount,
+        brand,
+        category_id,
+    }: ICreateCarDTO): Promise<void> {
+        const cars = new Car();
+
+        Object.assign(cars, {
+            name,
+            description,
+            daily_rate,
+            license_plate,
+            fine_amount,
+            brand,
+            category_id,
+        });
+
+        this.cars.push(cars);
     }
 }
 
