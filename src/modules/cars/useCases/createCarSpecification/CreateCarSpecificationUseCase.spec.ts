806a4ffc400b6ac6540ab47e988a2df4721137f4
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-25 15:42:57 -0300

    chore: not existent car specification

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../shared/errors/AppError';
 import { CarsRepositoryInMemory } from '../../repository/in-memory/CarsRepositoryInMemory';
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
@@ -11,5 +12,26 @@ describe('Create Car Specification', () => {
             carsRepository
         );
     });
-    it('should be able to add a new specification to the car', async () => {});
+    it('should not be able to add a new specification to a now-existent car', async () => {
+        expect(async () => {
+            const car_id = '123';
+
+            const specifications_id = ['23123', '23123123'];
+
+            await createCarSpecificationUseCase.execute({
+                car_id,
+                specifications_id,
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+    // it('should be able to add a new specification to the car', async () => {
+    //     const car_id = '123';
+
+    //     const specifications_id = ['23123', '23123123'];
+
+    //     await createCarSpecificationUseCase.execute({
+    //         car_id,
+    //         specifications_id,
+    //     });
+    // });
 });
