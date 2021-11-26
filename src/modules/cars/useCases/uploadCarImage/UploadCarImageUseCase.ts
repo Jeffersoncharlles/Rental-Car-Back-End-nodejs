3e9f02e61cbbd289f823a95d8ee74991ca968620
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-26 11:39:24 -0300

    chore: create carImagesUsecase upload

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,3 +1,24 @@
-class UploadCarImageUseCase {}
+import { inject, injectable } from 'tsyringe';
+
+import { ICarsImagesRepository } from '../../repository/ICarsImagesRepository';
+
+interface IRequest {
+    car_id: string;
+    images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+    constructor(
+        @inject('CarsImagesRepository')
+        private carsImagesRepository: ICarsImagesRepository
+    ) {}
+
+    async execute({ car_id, images_name }: IRequest): Promise<void> {
+        images_name.map(async (img) => {
+            await this.carsImagesRepository.create(car_id, img);
+        });
+    }
+}
 
 export { UploadCarImageUseCase };
