84e468f296ca27eb7fd35ca086c327127a6b44ed
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-26 11:52:37 -0300

    chore: create uploadcarsimagecontroller handle

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -1,3 +1,28 @@
-class UploadCarImageController {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UploadCarImageUseCase } from './UploadCarImageUseCase';
+
+interface IFile {
+    filename: string;
+}
+
+class UploadCarImageController {
+    async handle(req: Request, res: Response): Promise<Response> {
+        const { id } = req.params;
+        const images = req.files as IFile[];
+
+        const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
+
+        const images_name = images.map((file) => file.filename);
+
+        await uploadCarImageUseCase.execute({
+            car_id: id,
+            images_name,
+        });
+
+        return res.status(201).send();
+    }
+}
 
 export { UploadCarImageController };
