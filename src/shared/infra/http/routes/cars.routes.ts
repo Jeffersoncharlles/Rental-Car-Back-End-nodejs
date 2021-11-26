48fea757eb53517537df54b99fc97d82872340ba
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-26 11:59:06 -0300

    chore: create route images carupload

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../../../../config/upload';
 import { CreateCarController } from '../../../../modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { GetAvailableCarsController } from '../../../../modules/cars/useCases/GetAvailableCars/GetAvailableCarsController';
+import { UploadCarImageController } from '../../../../modules/cars/useCases/uploadCarImage/UploadCarImageController';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -11,6 +14,9 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const getAvailableCarsController = new GetAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
+
+const upload = multer(uploadConfig.upload('./tmp/cars'));
 
 /*= ================================================================================== */
 /*= ================================================================================== */
@@ -29,6 +35,14 @@ carsRoutes.post(
     ensureAuthenticated,
     ensureAdmin,
     createCarSpecificationController.handle
+);
+/*= ================================================================================== */
+carsRoutes.post(
+    '/images/:id',
+    ensureAuthenticated,
+    ensureAdmin,
+    upload.array('images'),
+    uploadCarImageController.handle
 );
 /*= ================================================================================== */
 
