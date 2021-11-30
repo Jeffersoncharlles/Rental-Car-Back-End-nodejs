3c49866ccfbcf95f5ccef25e9930b4bd71fad483
jeffersoncharlles <contato@jefferdeveloper.com>
2021-11-30 13:08:09 -0300

    chore: token created ok forget

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,13 +1,45 @@
 import { inject, injectable } from 'tsyringe';
+import { v4 as uuidV4 } from 'uuid';
+
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+import { IUsersTokensRepository } from '../../repositories/IUsersTokensRepository';
 
 @injectable()
 class SendForgotPasswordMailUseCase {
     constructor(
-        @inject()
-        private
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('UsersTokensRepository')
+        private tokensRepository: IUsersTokensRepository,
+        @inject('DayjsDateProvider')
+        private dayProvider: IDateProvider
     ) {}
 
-    async execute(email: string) {}
+    async execute(email: string) {
+        const timeExpire = 3;
+
+        // verificando usuario
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new AppError('User does not exists!');
+        }
+
+        // token do link em uuid para ficar menor
+        const token = uuidV4();
+
+        // criando data de expiracao
+        const expiresDate = this.dayProvider.addHours(timeExpire);
+
+        await this.tokensRepository.create({
+            refresh_token: token,
+            user_id: user.id,
+            expires_date: expiresDate,
+        });
+    }
 }
 
 export { SendForgotPasswordMailUseCase };
--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -4,6 +4,7 @@ interface IDateProvider {
     dateNow(): Date;
     compareInDays(start: Date, end: Date): number;
     addDays(days: number): Date;
+    addHours(hours: number): Date;
 }
 
 export { IDateProvider };
